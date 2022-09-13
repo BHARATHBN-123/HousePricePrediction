f60db3b8ea4a813d2dfb025c21176b538d8117c8
BHARATH BN <bharathbn17498@gmail.com>
2022-09-13 20:43:39 +0530

    created flask api and added home.html page

--- app.py
+++ app.py
@@ -6,11 +6,20 @@ import numpy as np
 import pandas as pd  
 app = Flask(__name__)
 # Load the model
-regmodel = pickle.load(open('regmodel.pkl','wb'))
-scaler = pickle.load(open('regmodel.pkl','wb'))
+regmodel = pickle.load(open('/home/bharath/Desktop/ML_E2E/HousePricePrediction/regmodel.pkl','rb'))
+scaler = pickle.load(open('/home/bharath/Desktop/ML_E2E/HousePricePrediction/scaling.pkl','rb'))
 @app.route('/')
 def home():
     return render_template('home.html')
 @app.route('/predict_api',methods = ['post'])
 def predict_api():
-    
+    data = request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    output = regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+if __name__ == '__main__':
+    app.run(debug =True)
